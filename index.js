53e4de0c3f79859c0a768723b7662c25b9e75196
Carla Holcomb <carla.programmer@gmail.com>
2020-05-29 18:53:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 // Write your code here!
-let index = document.remove('main#main')
-let newHeader = document.createElement('h1#victory')
+document.querySelector("main#main").remove();
+let newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
